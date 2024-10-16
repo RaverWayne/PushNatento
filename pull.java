3d53396d848a7af61ffe505a06f5f6b8dcb7919f
RaverWayne <raverwayne123@gmail.com>
2024-10-17 00:11:51 +0800

    Create pull.java

---
+++ pull.java
@@ -0,0 +1,16 @@
+public class SpecialCharacters {
+  public static void main(String args[]) {
+     //The sequence \'  inserts a single quote in a string:
+      String txt = "It\'s alright.";
+        System.out.println(txt);
+    
+      //The sequence \"  inserts a double quote in a string:
+      String tx = "We are the so-called \"Vikings\" from the north.";
+      System.out.println(tx);
+    
+    //The sequence \\  inserts a single backslash in a string:
+    String xt = "The character \\ is called backslash.";
+    System.out.println(xt);
+  }
+}
+
